c880fc50fa1b1c74cdb0304b1a7c489dcd14b1ce
Cong <congjeezy@gmail.com>
2025-03-03 10:04:21 -0600

    added shopping cart features

---
+++ SportsStore.Domain/Entities/Cart.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Domain.Entities
+{
+    public class CartLine
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+    public class Cart
+    {
+        //initialize the cart
+        //getter for CartLine
+        //add new item
+        //if item exists increase quantity
+        //remove item
+        //compute total cost of all items in cart
+        //emptying cart
+        private List<CartLine> lineCollection = new List<CartLine>();
+
+        //acccess content of cart
+        public IEnumerable<CartLine> Lines { get { return lineCollection; } }
+
+        //add item
+        public void AddItem(Product product, int quantity)
+        {
+            CartLine line = Lines
+                .Where(p => p.Product.Id == product.Id)
+                .FirstOrDefault();
+
+            //add new item, else increase quantity
+            if (line == null)
+            {
+                lineCollection.Add(new CartLine
+                {
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        //remove item
+        public void RemoveLine(Product product)
+        {
+            lineCollection.RemoveAll(l => l.Product.Id == product.Id);
+        }
+
+        //compute total of items in cart
+        public decimal ComputeTotalValue()
+        {
+            return lineCollection.Sum(t => t.Product.Price * t.Quantity);
+        }
+
+        //empty or reset cart
+        public void Clear()
+        {
+            lineCollection.Clear();
+        }
+
+    }
+}
---
+++ SportsStore.WebUI/Models/CartIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Models
+{
+    public class CartIndexViewModel
+    {
+        public Cart cart { get; set; }
+        public string returnUrl { get; set; }
+    }
+}
